3086ee35d6dc54df3f40760b4ec6795f9d2e943e
davidadediji <davidadediji@gmail.com>
2022-06-29 04:49:38 +0100

    added property model

--- src/main/java/com/dufuna/berlin/davidadediji/HelloWorld.java
+++ src/main/java/com/dufuna/berlin/davidadediji/HelloWorld.java
@@ -1,20 +1,9 @@
 package com.dufuna.berlin.davidadediji;
 
-import com.dufuna.berlin.davidadediji.lekki.Address;
-import com.dufuna.berlin.davidadediji.lekki.enums.PropertyType;
-import com.dufuna.berlin.davidadediji.lekki.model.LekkiProperty;
 
 //top level package...
 public class HelloWorld {
     public static void main(String[] args) {
-        Address address = new Address();
-        address.setStreetAddress("some street");
-        address.setCity("lekki");
-
-        LekkiProperty david= new LekkiProperty();
-        david.setId(1);
-        david.setAddress(address);
-        david.setType(PropertyType.FLAT);
 
         System.out.println("Hello, World!");
     }
--- src/main/java/com/dufuna/berlin/davidadediji/lekki/model/LekkiProperty.java
+++ src/main/java/com/dufuna/berlin/davidadediji/lekki/model/LekkiProperty.java
@@ -21,10 +21,101 @@ public class LekkiProperty {
     private Date validTo;
 
 
-    private boolean verify;
+    public int getPropertyId() {
+        return propertyId;
+    }
 
+    public void setPropertyId(int propertyId) {
+        this.propertyId = propertyId;
+    }
 
+    public Address getPropertyAddress() {
+        return propertyAddress;
+    }
 
+    public void setPropertyAddress(Address propertyAddress) {
+        this.propertyAddress = propertyAddress;
+    }
+
+    public PropertyType getType() {
+        return type;
+    }
+
+    public void setType(PropertyType type) {
+        this.type = type;
+    }
+
+    public int getBedroomNumber() {
+        return bedroomNumber;
+    }
+
+    public void setBedroomNumber(int bedroomNumber) {
+        this.bedroomNumber = bedroomNumber;
+    }
+
+    public int getSittingRoomNumber() {
+        return sittingRoomNumber;
+    }
+
+    public void setSittingRoomNumber(int sittingRoomNumber) {
+        this.sittingRoomNumber = sittingRoomNumber;
+    }
+
+    public int getKitchenNumber() {
+        return kitchenNumber;
+    }
+
+    public void setKitchenNumber(int kitchenNumber) {
+        this.kitchenNumber = kitchenNumber;
+    }
+
+    public int getBathroomNumber() {
+        return bathroomNumber;
+    }
+
+    public void setBathroomNumber(int bathroomNumber) {
+        this.bathroomNumber = bathroomNumber;
+    }
+
+    public int getToiletNumber() {
+        return toiletNumber;
+    }
+
+    public void setToiletNumber(int toiletNumber) {
+        this.toiletNumber = toiletNumber;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(Date validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public Date getValidTo() {
+        return validTo;
+    }
+
+    public void setValidTo(Date validTo) {
+        this.validTo = validTo;
+    }
 
 
 }
